f0756f0147d06aa61e6f62421723a51821898a90
connorbodin <connor.e.bodin@gmail.com>
2023-03-06 21:20:08 -0600

    tailwind setup

---
+++ portfolio-react-app/src/App.js
@@ -0,0 +1,23 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className=''>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="font-bold"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
