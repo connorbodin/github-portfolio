a46d03bb52cf17b6ab7a88752a8d6a9706a46dee
connorbodin <connor.e.bodin@gmail.com>
2023-03-20 18:37:34 -0500

    committ after reopen

--- portfolio-react-app/src/components/Home.js
+++ portfolio-react-app/src/components/Home.js
@@ -1,32 +1,35 @@
 import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
 
-const About = () => {
+const Home = () => {
   return (
-    <div name='about' className='w-full h-screen bg-[#e1e1e1] text-gray-500'>
-      <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-          <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
-              About Me
-            </p>
-          </div>
-          <div></div>
-          </div>
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi, My name is Connor Bodin. a full stack software engineer based out of Austin, TX</p>
-            </div>
-            <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>  
-            </div>
-          </div>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6] hover:border-b-4 duration-300 p-3 border-pink-600'>
+          Connor Bodin
+        </h1>
+        
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          I'm a Full Stack Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          I’m a full-stack developer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I’m focused on
+          building responsive full-stack web applications.
+        </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex duration-500 items-center hover:bg-pink-600 hover:border-pink-600'>
+            View Work
+            <span className='group-hover:rotate-90 duration-500'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default About;
+export default Home;
--- portfolio-react-app/src/components/Navbar.js
+++ portfolio-react-app/src/components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-500'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-500 cursor-pointer'>
       <div>
         <img src={Logo} alt='Logo Image' style={{ width: '100px' }} />
       </div>
@@ -30,22 +30,22 @@ const Navbar = () => {
           </Link>
         </li>
         <li  className='p-2'>
-          <Link to='about' smooth={true} duration={500}>
+          <Link to='about' className='hover:text-gray-300 duration-200' smooth={true} duration={500}>
             About
           </Link>
         </li>
         <li  className='p-2'>
-          <Link to='skills' smooth={true} duration={500}>
+          <Link to='skills' className='hover:text-gray-300 duration-200' smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li  className='p-2'>
-          <Link to='work' smooth={true} duration={500}>
+          <Link to='work' className='hover:text-gray-300 duration-200' smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li  className='p-2'>
-          <Link to='contact' smooth={true} duration={500}>
+        <li  className='p-2' >
+          <Link to='contact' className='hover:text-gray-300 duration-200' smooth={true} duration={500}>
             Contact
           </Link>
         </li>
