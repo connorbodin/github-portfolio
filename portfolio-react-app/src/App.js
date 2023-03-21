18f0d764fc25b08c03b360b5ffa619ce60b8f7ae
connorbodin <connor.e.bodin@gmail.com>
2023-03-20 19:24:03 -0500

    commit

--- portfolio-react-app/src/App.js
+++ portfolio-react-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Skills from './components/Skills.js';
 import Navbar from '../src/components/Navbar.js'
 import Home from '../src/components/Home.js'
 function App() {
@@ -8,6 +8,7 @@ function App() {
     <>
       <Navbar />
       <Home />  
+      <Skills />
     </>
   );
 }
